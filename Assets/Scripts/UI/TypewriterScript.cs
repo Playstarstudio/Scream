91e5bcbb34b498c8f659b37928edd244f7310e16
johnnywhiteux <johnnywhite.ux@gmail.com>
2026-04-25 21:31:13 -0400

    Vignette and Typewriter
    pushing typewriter script and vignette UI

---
+++ Assets/Scripts/UI/TypewriterScript.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class TypewriterScript : MonoBehaviour
+{
+
+    [SerializeField] private string testText;
+
+    private TMP_Text textBox;
+    private int currentVisibleCharacterIndex;
+    private Coroutine typewriterCoroutine;
+
+    private WaitForSeconds simpleDelay;
+    private WaitForSeconds interpunctuationDelay;
+
+    [SerializeField] private float characterPerSecond = 12;
+    [SerializeField] private float floatinterpunctuationDelay = 0.5f;
+
+    private void Awake()
+    {
+        textBox = GetComponent<TMP_Text>();
+
+        simpleDelay = new WaitForSeconds(1 / characterPerSecond);
+        interpunctuationDelay = new WaitForSeconds(floatinterpunctuationDelay);
+    }
+
+    private void Start()
+    {
+        SetText(testText);
+    }
+
+    public void SetText(string Text)
+    {
+        if (typewriterCoroutine != null)
+            StopCoroutine(typewriterCoroutine);
+
+        textBox.text = Text;
+        textBox.maxVisibleCharacters = 0;
+        currentVisibleCharacterIndex = 0;
+
+        typewriterCoroutine = StartCoroutine(routine: Typewriter());
+
+    }
+
+    private IEnumerator Typewriter()
+    {
+        TMP_TextInfo textInfo = textBox.textInfo;
+
+        while (currentVisibleCharacterIndex < textInfo.characterCount + 1)
+        {
+
+            char character = textInfo.characterInfo[currentVisibleCharacterIndex].character;
+
+            textBox.maxVisibleCharacters++;
+
+            if (character == '?' || character == '!' || character == ',' || character == '.' || character == ';' || character == ':' || character == '-')
+            {
+                yield return interpunctuationDelay;
+            }
+            else
+            {
+                yield return simpleDelay;
+            }
+
+            currentVisibleCharacterIndex++;
+
+        }
+    }
+
+}
