a2c71702222a8353eca596952b17803c8e0e2136
Harrison Leon <harrison.leon@flat2vr.com>
2026-04-27 00:37:00 -0700

    When you click the backpack, the inventory slots will slide in/out
    TODO: tie this backpack animation to a gesture instead of a click

---
+++ Assets/Scripts/UI/BackpackInventoryScript.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class BackpackInventoryScript : MonoBehaviour
+    {
+        public RectTransform inventory;
+        
+        private float _animationDuration = 0.4f;
+     
+        [Tooltip("The easing curve for the animation")]
+        private AnimationCurve _easeCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+
+        public Vector2 hiddenOffset;
+        private Vector2 _closedPosition;
+        private Vector2 _openPosition;
+     
+        private bool _isOpen = false;
+        private bool _isAnimating = false;
+        private Coroutine _activeCoroutine;
+
+        private void Awake()
+        {
+            _openPosition = inventory.anchoredPosition;
+            _closedPosition = _openPosition + hiddenOffset;
+
+            inventory.anchoredPosition = _closedPosition;
+            inventory.gameObject.SetActive(false);
+        }
+        
+        public void OnGesture()
+        {
+            Debug.Log("On Click");
+            if (_isAnimating) return;
+     
+            if (_isOpen)
+                SlideDown();
+            else
+                SlideUp();
+        }
+
+        private void SlideUp()
+        {
+            _isOpen = true;
+            inventory.gameObject.SetActive(true);
+            if (_activeCoroutine != null) StopCoroutine(_activeCoroutine);
+            _activeCoroutine = StartCoroutine(AnimateBackpack(_closedPosition, _openPosition));
+        }
+
+        private void SlideDown()
+        {
+            _isOpen = false;
+            if (_activeCoroutine != null) StopCoroutine(_activeCoroutine);
+            _activeCoroutine = StartCoroutine(AnimateBackpack(_openPosition, _closedPosition));
+        }
+
+        private IEnumerator AnimateBackpack(Vector2 from, Vector2 to)
+        {
+            _isAnimating = true;
+            var elapsed = 0f;
+     
+            while (elapsed < _animationDuration)
+            {
+                elapsed += Time.deltaTime;
+                var t = Mathf.Clamp01(elapsed / _animationDuration);
+                var curvedT = _easeCurve.Evaluate(t);
+                inventory.anchoredPosition = Vector2.LerpUnclamped(from, to, curvedT);
+                yield return null;
+            }
+
+            inventory.anchoredPosition = to;
+            if (!_isOpen)
+            {
+                inventory.gameObject.SetActive(false);
+            }
+            _isAnimating = false;
+        }
+    }
+}
