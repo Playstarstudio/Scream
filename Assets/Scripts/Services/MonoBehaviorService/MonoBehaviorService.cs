5de622e7ccad76c870d89c8e7a88b7d0368f1dbd
Patrick Moffett <patrick@moffett.dev>
2026-04-27 22:25:25 -0700

    Doors

---
+++ Assets/Scripts/Services/MonoBehaviorService/MonoBehaviorService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Services
+{
+    public class MonoBehaviorService : MonoBehaviour, IService
+    {
+        public event Action AwakeEvent;
+        public event Action StartEvent;
+        public event Action UpdateEvent;
+        public event Action LateUpdateEvent;
+        public event Action FixedUpdateEvent;
+        private void Awake()
+        {
+            AwakeEvent?.Invoke();
+        }
+
+        private void Start()
+        {
+            StartEvent?.Invoke();
+        }
+
+        private void Update()
+        {
+            UpdateEvent?.Invoke();
+        }
+
+        private void LateUpdate()
+        {
+            LateUpdateEvent?.Invoke();
+        }
+
+        private void FixedUpdate()
+        {
+            FixedUpdateEvent?.Invoke();
+        }
+        
+    }
+}
