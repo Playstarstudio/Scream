a22278f15aa4f43f4fe8d26566881ce2531cf6bd
Harrison Leon <harrison.leon@flat2vr.com>
2026-04-28 00:08:40 -0700

    Implement distance checker for UI lantern
    - If the lantern is close enough, obscuring particle effects in the UI will disappear

---
+++ Assets/Scripts/UI/InventoryLanternHighlight.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class InventoryLanternHighlight : MonoBehaviour
+{
+    [SerializeField] private RectTransform lantern;
+    [SerializeField] private ParticleSystem[] inventoryParticles;
+    private RectTransform[] _particleTransforms;
+
+    [SerializeField] private float lanternDistance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _particleTransforms = new RectTransform[inventoryParticles.Length];
+        for (int i = 0; i < inventoryParticles.Length; i++)
+        {
+            _particleTransforms[i] = inventoryParticles[i].GetComponent<RectTransform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < _particleTransforms.Length; i++)
+        {
+            var distanceToLantern = Vector2.Distance(_particleTransforms[i].position, lantern.position);
+            Debug.Log("DistanceToLantern: " + distanceToLantern);
+            if (distanceToLantern <= lanternDistance)
+            {
+                inventoryParticles[i].Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            }
+            else if (!inventoryParticles[i].isPlaying)
+            {
+                inventoryParticles[i].Play();
+            }
+        }
+    }
+}
