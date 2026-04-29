9e81226fc13f5ff02568502a04a4cbac43b43fde
Patrick Moffett <patrick@moffett.dev>
2026-04-28 22:58:24 -0700

    Gesture Opener

---
+++ Assets/Scripts/Editor/FirstAltarGestureEditor.cs
@@ -0,0 +1,51 @@
+﻿using UI;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(FirstAltarGestureScript))]
+    public class FirstAltarGestureEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            FirstAltarGestureScript altar = (FirstAltarGestureScript)target;
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Debug / Testing", EditorStyles.boldLabel);
+
+            if (!Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox("Enter Play Mode to use the testing buttons.", MessageType.Info);
+                return;
+            }
+
+            // Status
+            EditorGUILayout.LabelField("Matches Placed", altar.IsMatchesPlaced ? "Yes" : "No");
+            EditorGUILayout.LabelField("Candle Placed", altar.IsCandlePlaced ? "Yes" : "No");
+            EditorGUILayout.LabelField("Matches Lit", altar.IsMatchesLit ? "Yes" : "No");
+
+            EditorGUILayout.Space(4);
+
+            // Buttons
+            EditorGUI.BeginDisabledGroup(altar.IsMatchesPlaced);
+            if (GUILayout.Button("Place Matches", GUILayout.Height(28)))
+            {
+                altar.PlaceMatches();
+            }
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUI.BeginDisabledGroup(altar.IsCandlePlaced);
+            if (GUILayout.Button("Place Candle", GUILayout.Height(28)))
+            {
+                altar.PlaceCandle();
+            }
+            EditorGUI.EndDisabledGroup();
+
+            Repaint();
+        }
+    }
+}
+
