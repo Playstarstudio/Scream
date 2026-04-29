20ff4292774427beadf0fd6804f67594e1a408c6
Harrison Leon <harrison.leon@flat2vr.com>
2026-04-29 01:18:54 -0700

    Cursor become one with inventory movements

--- Assets/Scripts/UI/DraggableItemWidget.cs
+++ Assets/Scripts/UI/DraggableItemWidget.cs
@@ -2,12 +2,17 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class DraggableItemWidget : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
     // Script used for drag & dropping Inventory Items!
+
+    public RectTransform cursorTransform;
+    public Image cursorImage;
+    public Canvas parentCanvas;
 
     public Image _image;
     public GameObject invItem;
@@ -26,6 +31,14 @@ public class DraggableItemWidget : MonoBehaviour, IBeginDragHandler, IDragHandle
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        var clickedSprite = _image.sprite;
+        if (clickedSprite != null)
+        {
+            cursorImage.sprite = clickedSprite;
+            cursorImage.gameObject.SetActive(true);
+        }
+        
+        
         // parentAfterDrag = transform.parent;
         //
         // transform.SetParent(rootTransform); // set parent to canvas
@@ -36,11 +49,21 @@ public class DraggableItemWidget : MonoBehaviour, IBeginDragHandler, IDragHandle
 
     public void OnDrag(PointerEventData eventData)
     {
+        Vector2 position;
+        // Converts screen point (mouse) to local point relative to the Canvas
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            parentCanvas.transform as RectTransform, 
+            Mouse.current.position.value,
+            parentCanvas.worldCamera, 
+            out position);
+            
+        cursorTransform.anchoredPosition = position;
         // transform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        cursorImage.gameObject.SetActive(false);
         GameObject hitObject = eventData.pointerCurrentRaycast.gameObject;
         if (hitObject != null)
         {
